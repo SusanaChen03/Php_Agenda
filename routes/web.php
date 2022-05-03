6bcc7bf51e3a484a027d6d491965401db339f366
SusanaChen03 <grupochen@hotmail.com>
2022-05-03 10:41:16 +0200

    prueba routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/', function () {
+//     return [
+//         "nombre" => "Antonio",
+//         "apellido" => "Tarazona"
+//     ];
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'agenda2';
 });
