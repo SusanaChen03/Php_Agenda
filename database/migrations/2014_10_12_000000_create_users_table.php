019f862a8940b57e01865401a838e10e51433bf7
SusanaChen03 <grupochen@hotmail.com>
2022-05-03 18:49:44 +0200

    create new migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('isActive')->default(true);     //activo o no //si ya esta hecha la migracion. se borra y se vuelve  
+            //$table->boolean('isActive')->default(true);     //activo o no //si ya esta hecha la migracion. se borra y se vuelve  
             $table->rememberToken();
             $table->timestamps();
         });
